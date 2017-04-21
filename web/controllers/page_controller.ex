636b6ab1b66533a97cccbc357e3938cb20c6665f
Josh Rendek <josh@bluescripts.net>
2017-04-20 23:06:10 -0400

    add forwarded for

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,7 +4,13 @@ defmodule Ifcfg.PageController do
   require Logger
 
   def index(conn, _params) do
-    remote_ip = conn.remote_ip |> Tuple.to_list |> Enum.join(".")
+    remote_ips = Plug.Conn.get_req_header(conn, "x-forwarded-for")
+    remote_ip = List.first(remote_ips)
+    unless remote_ip do
+      remote_ip_as_tuple = conn.remote_ip
+      remote_ip = Enum.join(Tuple.to_list(remote_ip_as_tuple), ".")
+    end
+
     user_agent = Plug.Conn.get_req_header(conn, "user-agent")
     Logger.info "#{remote_ip} : #{user_agent}"
 
